9f9383756968f5661f15c93b8932f9a9ca2f8860
Alexander Leitner <leitnersalex@gmail.com>
2019-06-28 10:44:13 -0400

    Add comments

--- main.go
+++ main.go
@@ -13,8 +13,13 @@ func main() {
 }
 
 // todo: use a linear equations solver instead of brute forcing it.
+// Wc - Current ethanol Ratio
+// Fc - Current gallons of fuel
+// Wt - Optimal ethanol Ratio
+// Ft - tank number of gallons
+// W1 - ethanol Ratio of fuel 1
+// W2 - ethanol Ratio of fuel 2
 func bruteForceCalculate(Wc, Fc, Wt, Ft, W1, W2 float64) (fuel1, fuel2, resultRatio float64) {
-	fmt.Println(Wc, Fc, Wt, Ft, W1, W2)
 	if Fc > Ft {
 		return fuel1, fuel2, resultRatio
 	}
@@ -25,7 +30,6 @@ func bruteForceCalculate(Wc, Fc, Wt, Ft, W1, W2 float64) (fuel1, fuel2, resultRa
 		ratio := (f1*W1 +f2*W2 + Wc*Fc) / (Fc + f1 + f2)
 
 		if math.Abs(Wt - ratio) < math.Abs(Wt - resultRatio) {
-
 			resultRatio = ratio
 			fuel1 = i
 			fuel2 = Ft - Fc - i
